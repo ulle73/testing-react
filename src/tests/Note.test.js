9e9a99adc7b55dd249e57b8867a27a913c64e7f1
Patrik Nygren <patriknygren82@gmail.com>
2024-04-25 11:05:14 +0200

    starting point

---
+++ src/tests/Note.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Note from "../components/Note";
+
+test("renders content", () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
+
+  render(<Note note={note} />);
+
+  const element = screen.getByText(
+    "Component testing is done with react-testing-library"
+  );
+  expect(element).toBeDefined();
+});
